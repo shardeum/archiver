beaea088e395e01e31aa655d72b0354da8bf57ef
Zachary Belford <belfordz66@gmail.com>
2025-05-09 10:58:51 -0700

    fix: remove comment

--- src/Data/Collector.ts
+++ src/Data/Collector.ts
@@ -880,7 +880,6 @@ export const storeReceiptData = async (
       processedReceiptsMap.set(txId, tx.timestamp)
       receiptsInValidationMap.delete(txId)
       if (missingReceiptsMap.has(txId)) missingReceiptsMap.delete(txId)
-      // Store beforeStates only for globalModification tx, or for cycles before the activation cycle, or if config.storeReceiptBeforeStates is true for cycles after the activation
       receipt.beforeStates = globalModification || config.storeReceiptBeforeStates ? receipt.beforeStates : [] // Store beforeStates for globalModification tx, or if config.storeReceiptBeforeStates is true
       let executionShardKey: string
       if (globalModification) {
